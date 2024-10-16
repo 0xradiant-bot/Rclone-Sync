b9d48e4195145481e3218a511456cf8c2478c5bd
0xRad1ant <168742287+0xradiant-bot@users.noreply.github.com>
2024-10-17 00:51:20 +0530

    doing shit in error.sh

---
+++ error.sh
@@ -0,0 +1,147 @@
+#!/bin/sh
+
+# Colors :
+Green='\033[0;32m'        # Green
+Cyan='\033[1;36m'        # Cyan
+No='\e[0m'
+
+clear
+echo -e "${Cyan} ____    __    ____   ___  _   _   "
+echo -e "${Cyan}(  _ \  /__\  (_  _) / __)( )_( )  "
+echo -e "${Cyan} ) _ < /(__)\   )(  ( (__  ) _ (   "
+echo -e "${Cyan}(____/(__)(__) (__)  \___)(_) (_)  "
+echo -e "${Cyan} __  __    __    _  _              "
+echo -e "${Cyan}(  \/  )  /__\  ( \( )             "
+echo -e "${Cyan} )    (  /(__)\  )  (              "
+echo -e "${Cyan}(_/\/\_)(__)(__)(_)\_)             "
+echo -e "${Green}By: @0xRad1ant${No}"
+
+# Add BlackArch repository
+if ! grep -q "blackarch" /etc/pacman.conf; then
+    echo "[blackarch]" >> /etc/pacman.conf
+    echo "Server = https://blackarch.org/blackarch/\$repo/os/\$arch" >> /etc/pacman.conf
+    echo "Repository added successfully."
+else
+    echo "BlackArch repository is already added."
+fi
+
+# Update package database
+pacman -Syu --noconfirm
+
+# Install Yay if not installed
+if ! command -v yay &> /dev/null; then
+    echo "Installing Yay..."
+    sudo pacman -S --needed git base-devel --noconfirm
+    git clone https://aur.archlinux.org/yay.git
+    cd yay || exit
+    makepkg -si --noconfirm
+    cd .. && rm -rf yay
+else
+    echo "Yay is already installed."
+fi
+
+# Function to install packages if not already installed
+install_package() {
+    package=$1
+    if ! pacman -Qq | grep -qw "$package"; then
+        if yay -Qq | grep -qw "$package"; then
+            echo "$package is available in AUR, installing..."
+            yay -S --noconfirm "$package"
+        else
+            echo "$package is not installed, installing..."
+            sudo pacman -S --needed --noconfirm "$package"
+        fi
+    else
+        echo "$package is already installed."
+    fi
+}
+
+# System Utilities
+install_package "git"
+install_package "wget"
+install_package "curl"
+install_package "fastfetch"
+install_package "btop"
+
+# Networking
+install_package "networkmanager"
+install_package "nmap"
+install_package "net-tools"
+install_package "wireshark"
+install_package "wifite"
+
+# File System Utilities
+install_package "ntfs-3g"
+install_package "exfat-utils"
+
+# Terminal & Shell
+install_package "tmux"
+install_package "bash-completion"
+
+# Fonts
+install_package "ttf-dejavu"
+install_package "noto-fonts"
+install_package "noto-fonts-emoji"
+
+# Applications
+install_package "firefox"
+install_package "zen-browser-bin"
+install_package "vlc"
+install_package "libreoffice-fresh"
+install_package "eog"
+install_package "partitionmanager"
+install_package "spotify"
+install_package "obs-studio"
+install_package "obsidian"
+install_package "vesktop"
+install_package "termius"
+
+# Other
+install_package "reflector"
+install_package "rclone"
+install_package "pacman-mirrorlist"
+install_package "openvpn"
+install_package "logwatch"
+install_package "man-db"
+install_package "hash-identifier"
+install_package "hashcat"
+install_package "google-chrome"
+install_package "fast"
+install_package "downgrade"
+install_package "cloudflare-warp-bin"
+install_package "arandr"
+install_package "archlinux-keyring"
+install_package "aircrack-ng"
+
+# Development
+install_package "visual-studio-code-bin"
+install_package "nvim"
+install_package "nodejs"
+install_package "npm"
+
+# Install find-the-command
+install_package "find-the-command"
+
+# Enable UFW and set up basic rules
+sudo ufw enable
+sudo systemctl enable ufw
+
+# Check SSH and disable it
+if systemctl is-active --quiet sshd; then
+    echo "Disabling SSH service..."
+    sudo systemctl stop sshd.service
+    sudo systemctl disable sshd.service
+else
+    echo "SSHD service is not active."
+fi
+
+# Adding find-the-command source to .zshrc or .bashrc
+if [ -f "$HOME/.zshrc" ]; then
+    echo "source /usr/share/doc/find-the-command/ftc.zsh" >> "$HOME/.zshrc"
+    echo "Added find-the-command to .zshrc"
+elif [ -f "$HOME/.bashrc" ]; then
+    echo "source /usr/share/doc/find-the-command/ftc.bash" >> "$HOME/.bashrc"
+    echo "Added find-the-command to .bashrc"
+fi
+
+echo "All installations completed and configurations are set!"
